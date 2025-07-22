9773f2ca9b1d79785810f9e9d1eb40dfee66be1d
RnD-Emb-IndoWings <vayam.jain@indowings.com>
2025-07-22 13:39:36 +0530

    Update i2c_connection_rpi.py

--- i2c_connection_rpi.py
+++ i2c_connection_rpi.py
@@ -1,6 +1,8 @@
-#-------------------------------------------------------------------------------Code for I2C connection with RPI and SF000/B--------------------------------------------------------------------------------# 
+#--------------------------------------------Code for I2C connection with RPI and SF000/B--------------------------------------------------------------------------------# 
 import time
 import smbus2
+import logging
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
 I2C_BUS = 1
 #I2C_ADDR = 0x66
@@ -16,19 +18,19 @@ def read_distance_cm(bus, addr):
         dist_cm = (data[1] << 8) + data[0]
         return dist_cm
     except Exception as e:
-        print(f"[ERROR] Failed to read from SF000: {e}")
+        logging.error(f"Failed to read from SF000: {e}")
         return None
 
 def main():
     bus = smbus2.SMBus(I2C_BUS)
-    print("[INFO] Starting SF000 LiDAR I2C reader...")
+    print("[INFO] Starting IW-SF000/B LiDAR I2C reader...")
 
     try:
         while True:
             data = bus.read_i2c_block_data(I2C_ADDR, 0x00, 2)
             dist_cm = (data[0] << 8) + data[1]  # High byte first, low byte second
             dist_m = dist_cm / 100.0
-            print(f"[DISTANCE] {dist_m:.2f} m")
+            logging.info(f"[DISTANCE] {dist_m:.2f} m")
             time.sleep(0.1)
     except KeyboardInterrupt:
         print("Stopped.")
